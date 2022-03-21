61ec008551dd501b37405ade9c33443ae6c6eb9d
songhaibei <songhaibei7305@163.com>
2022-03-21 22:50:37 +0800

    使用AbsoluteLayout实现平移控件

--- MyApplication018/app/src/main/java/com/jingwei/songhaibei/myapplication018/MainActivity.java
+++ MyApplication018/app/src/main/java/com/jingwei/songhaibei/myapplication018/MainActivity.java
@@ -3,12 +3,40 @@ package com.jingwei.songhaibei.myapplication018;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.AbsoluteLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView myTextView;
+    public static int x = 0;
+    public static int y = 0;
+    Handler myHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            if (msg.what == 0x123) {
+                AbsoluteLayout.LayoutParams params =
+                        new AbsoluteLayout.LayoutParams(256, 256, x++ % 756, y++ % 756);
+                myTextView.setLayoutParams(params);
+            }
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myTextView = (TextView) findViewById(R.id.myTextView);
+        new Timer().schedule(new TimerTask() {           //周期性地改变控件的坐标
+            @Override
+            public void run() {
+                myHandler.sendEmptyMessage(0x123);
+            }
+        }, 0, 10);
     }
 }
