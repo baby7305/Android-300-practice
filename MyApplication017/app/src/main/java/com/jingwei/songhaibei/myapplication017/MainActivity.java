d60fffbf996f95850ed83f72ef081598ba74a8e2
songhaibei <songhaibei7305@163.com>
2022-03-21 22:43:33 +0800

    使用TabHost创建选项卡

--- MyApplication017/app/src/main/java/com/jingwei/songhaibei/myapplication017/MainActivity.java
+++ MyApplication017/app/src/main/java/com/jingwei/songhaibei/myapplication017/MainActivity.java
@@ -2,13 +2,27 @@ package com.jingwei.songhaibei.myapplication017;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends TabActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TabHost myTabhost = getTabHost();
+        //加载TabHost布局
+        LayoutInflater.from(this).inflate(R.layout.activity_main,
+                myTabhost.getTabContentView(), true);
+        //添加第一个标签页
+        myTabhost.addTab(myTabhost.newTabSpec("tab1")
+                .setIndicator("黎明之战").setContent(R.id.tab01));
+        //添加第二个标签页
+        myTabhost.addTab(myTabhost.newTabSpec("tab2")
+                .setIndicator("权力之眼").setContent(R.id.tab02));
+        //添加第三个标签页
+        myTabhost.addTab(myTabhost.newTabSpec("tab3")
+                .setIndicator("圆梦巨人").setContent(R.id.tab03));
     }
 }
